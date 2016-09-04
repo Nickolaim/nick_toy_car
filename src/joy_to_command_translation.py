a66a7181dba1ddf8fc980873473db7b507c2c29a
Nick Medbeditskov <nickolaim@live.com>
2016-09-03 21:47:15 -0700

    Update joystick to commands

--- src/joy_to_command_translation.py
+++ src/joy_to_command_translation.py
@@ -5,10 +5,11 @@ import math
 
 def joystick_axes_to_degree(axis0, axis1):
     """
-    joystick_axes_to_degree(axis0, axis1)
-
     Convert joystick's axis 0 and axis 1 value into degrees.
-    For joystick in the center None is returned.
+
+    :param axis0: integer value for axis 0 from joystick
+    :param axis1: integer value for axis 1 from joystick
+    :return: For joystick in the center None is returned.
     Otherwise a degree is returned, where 0 degree points to North, 90 to East, -90 to West and 180/-180 to South
     """
     if axis0 == axis1 == 0:
@@ -21,3 +22,50 @@ def joystick_axes_to_degree(axis0, axis1):
     arc_tan_degree = arc_tan_degree + 360 if arc_tan_degree <= -180 else arc_tan_degree
 
     return arc_tan_degree
+
+
+def joystick_axes_to_command(axis0, axis1):
+    """
+    Convert joystick's axis 0 and axis 1 value into robot MOVE/STOP commands
+    :param axis0: integer value for axis 0 from joystick
+    :param axis1: integer value for axis 1 from joystick
+    :return: robot command as a string
+    """
+    degree = joystick_axes_to_degree(axis0, axis1)
+    return stop_command() if degree is None else move_command(degree)
+
+
+def stop_command():
+    """
+    :return: the command for stop
+    """
+    return 'STOP'
+
+
+def move_command(degree=None):
+    """
+    :param degree: degree, 0 pointing ahead.  If none, only string literal for the command is returned
+    :return: the command for move
+    """
+    return 'MOVE' + ('' if degree is None else str(int(degree)))
+
+
+def speed_command():
+    """
+    :return: the command for the speed
+    """
+    return 'SPEED'
+
+
+def speed_increase_command():
+    """
+    :return: the command for increasing the speed
+    """
+    return speed_command() + '+'
+
+
+def speed_decrease_command():
+    """
+    :return: the command for decreasing the speed
+    """
+    return speed_command() + '-'
--- src/tool_joystick_to_robot_cmd_translation.py
+++ src/tool_joystick_to_robot_cmd_translation.py
@@ -3,19 +3,22 @@
 import pipes
 import sys
 
+from joy_to_command_translation import joystick_axes_to_command, speed_increase_command, speed_decrease_command
+
 if len(sys.argv) != 2:
     arg0 = sys.argv[0]
     sys.exit('Converts output of jstest into commands for the robot.\n'
         'Usage: %s ''command_produces_jstest_output''\n'
+        'Example: ./test_joystick_to_command_translation.py ''jstest /dev/input/js0''\n'
         % arg0)
 
-cmd = sys.argv[1]
-print 'Command:' + cmd
+pipe_cmd = sys.argv[1]
+print 'Command:' + pipe_cmd
 
-marker_length = len(' X:') + 1
+marker_length = len(' X:')
 
 t = pipes.Template()
-t.append(cmd, '--')
+t.append(pipe_cmd, '--')
 f = t.open('', 'r')
 
 while True:
@@ -23,6 +26,7 @@ while True:
     if not line:
         print 'Finished reading'
         break
+
     pos2 = 0
     while True:
         posAxis = line.find('Axes:', pos2)
@@ -33,6 +37,14 @@ while True:
             break
         v1 = int(line[pos0 + marker_length:pos1])
         v2 = int(line[pos1 + marker_length:pos2])
-        print v1, v2
+        robot_cmd = joystick_axes_to_command(v1, v2)
+
+        if line.find('8:on') > -1:
+            robot_cmd += ' ' + speed_decrease_command()
+
+        if line.find('9:on') > -1:
+            robot_cmd += ' ' + speed_increase_command()
+
+        print "Axis0: {:>7}, Axis1: {:>7}, Command:{:<12}".format(v1, v2, robot_cmd)
 f.close()
 
